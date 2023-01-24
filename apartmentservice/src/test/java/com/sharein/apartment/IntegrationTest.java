7c715e79688f7ef2b562fa1e11bdaf0935031809
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-24 16:18:42 +0900

    Initial version of apartmentservice generated by generator-jhipster@7.9.3

---
+++ apartmentservice/src/test/java/com/sharein/apartment/IntegrationTest.java
@@ -0,0 +1,27 @@
+package com.sharein.apartment;
+
+import com.sharein.apartment.ApartmentserviceApp;
+import com.sharein.apartment.config.AsyncSyncConfiguration;
+import com.sharein.apartment.config.EmbeddedKafka;
+import com.sharein.apartment.config.EmbeddedRedis;
+import com.sharein.apartment.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ApartmentserviceApp.class, AsyncSyncConfiguration.class })
+@EmbeddedRedis
+@EmbeddedKafka
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
