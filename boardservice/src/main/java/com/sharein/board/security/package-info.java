48191512756e6290ac4bab065c930e9c959a1d39
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-27 19:08:53 +0900

    Initial version of boardservice generated by generator-jhipster@7.9.3

---
+++ boardservice/src/main/java/com/sharein/board/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.sharein.board.security;
---
+++ boardservice/src/main/java/com/sharein/board/web/rest/errors/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Specific errors used with Zalando's "problem-spring-web" library.
+ *
+ * More information on https://github.com/zalando/problem-spring-web
+ */
+package com.sharein.board.web.rest.errors;
